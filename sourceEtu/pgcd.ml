1c4a30b55d12cd1fe1cc1487d5ca5f4489b07c3a
Yanis Kouidri <yanis.kouidri@gmail.com>
2024-02-17 10:56:44 +0100

    Add absolute function and update pgcd function

--- sourceEtu/pgcd.ml
+++ sourceEtu/pgcd.ml
@@ -1,5 +1,16 @@
 (*  Exercice à rendre **)
-(*  PGCD int -> int -> int
+
+(* absolute : int -> int
+  Retourne la valeur absolu d'un entier
+  a int : entier dont on veut la valeur absolue
+  result : a si -a < 0  a sinon
+   *)
+   let absolute a =
+    if a < 0
+      then -a
+    else a
+
+(*  PGCD : int -> int -> int
    Retourne le PGCD de a et b 
    a int premier argument du PGCD
    b int second argument du PGCD
@@ -7,16 +18,15 @@
 let rec pgcd a b = 
     let aux a b =
     if (a = 0 || b = 0)
-    then max a b
+      then max a b
     else if (a > b) 
       then pgcd (a-b) b
       else pgcd a (b-a)
-    in aux (abs a) (abs b)   
-(*  TO DO : écrire abs en local *)
+    in aux (absolute a) (absolute b)   
 
 
-(*  TO DO : tests unitaires *)
- let%test _ = pgcd 0 0 = failwith "Undefined" 
+(*tests unitaires *)
+let%test _ = pgcd 0 0 = 0 (*NB : Normalement le PGCD(0,0) est indéfini*)
 let%test _ = pgcd 1 1 = 1
 let%test _ = pgcd 13 13 = 13
 let%test _ = pgcd 0 17 = 17
@@ -24,9 +34,14 @@ let%test _ = pgcd 17 0 = 17
 let%test _ = pgcd 18 4 = 2
 let%test _ = pgcd 4 18 = 2
 let%test _ = pgcd (-5) (-15) = 5
-
-
-
-
-
-
+let%test _ = pgcd (-1) (-1) = 1
+let%test _ = pgcd (-0) (-17) = 17
+let%test _ = pgcd 0 (-17) = 17
+let%test _ = pgcd (-0) 17 = 17
+let%test _ = pgcd 5 (-15) = 5
+let%test _ = pgcd (-5) 15 = 5
+let%test _ = pgcd 9 21 = 3
+let%test _ = pgcd (-9) 21 = 3
+let%test _ = pgcd 9 (-21) = 3
+let%test _ = pgcd (-9) (-21) = 3
+let%test _ = pgcd (-37) (-37) = 37
